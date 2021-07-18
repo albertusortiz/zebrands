58eff8a203d77eb021b2c9af34d985d2b52a1531
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-18 10:55:04 -0500

    Agregando descripcion del proyecto.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title='Sistema de Catalogo para Administrar Productos',
+            description='En este proyecto seremos capaces de gestionar usuarios y productos delimitando accesos con base al rol de cada usuario.',
+            version='1')
 
 @app.get('/')
 async def index():
