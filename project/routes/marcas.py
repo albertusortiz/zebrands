e6d2ca497814448b12cad1f5abccd33c5040a93c
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-21 22:41:04 -0500

    Token en /marcas

--- project/routes/marcas.py
+++ project/routes/marcas.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import HTTPException
 
 from ..database import Marca
@@ -8,30 +9,44 @@ from ..database import Marca
 from ..schemas import MarcaRequestModel
 from ..schemas import MarcaResponseModel
 
+from ..middleware import get_current_user
+
 router = APIRouter(prefix='/marcas')
 
 @router.post('', response_model=MarcaResponseModel)
-async def crear_marca(marca: MarcaRequestModel):
+async def crear_marca(marca: MarcaRequestModel, token: str = Depends(get_current_user)):
 
-    marca = Marca.create(
-        nombre=marca.nombre
-    )
+    if token.get("nivel") == 1:
 
-    return marca
+        marca = Marca.create(
+            nombre=marca.nombre
+        )
+
+        return marca
+
+    raise HTTPException(status_code=404, detail="Este usuario no tiene permisos para la petición.")
 
 @router.get('', response_model=List[MarcaResponseModel])
-async def obtener_marcas():
-    
-    marcas = Marca.select()
+async def obtener_marcas(token: str = Depends(get_current_user)):
 
-    return [marca for marca in marcas]
+    if token.get("nivel") == 1:
+        
+        marcas = Marca.select()
+
+        return [marca for marca in marcas]
+
+    raise HTTPException(status_code=404, detail="Este usuario no tiene permisos para la petición.")
 
 @router.get('/{marca_id}', response_model=MarcaResponseModel)
-async def obtener_marca_id(marca_id: int):
+async def obtener_marca_id(marca_id: int, token: str = Depends(get_current_user)):
 
-    marca_id = Marca.select().where(Marca.id == marca_id).first()
+    if token.get("nivel") == 1:
 
-    if marca_id is None:
-        raise HTTPException(status_code=404, detail='ID de marca no encontrada.')
+        marca_id = Marca.select().where(Marca.id == marca_id).first()
 
-    return marca_id
+        if marca_id is None:
+            raise HTTPException(status_code=404, detail='ID de marca no encontrada.')
+
+        return marca_id
+
+    raise HTTPException(status_code=404, detail="Este usuario no tiene permisos para la petición.")
